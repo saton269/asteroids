36f0204f727443f6018999de828808f14fc1c1f0
saton269 <saton269@gmail.com>
2025-06-22 17:16:51 -0400

    added asteroid field, game ends when running into an asteroid

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
+import sys
 from constants import *
-from player import *
+from circleshape import *
+from player import Player
+from asteroid import Asteroid
+from  asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -12,7 +16,11 @@ def main():
     dt = 0
     updatable = pygame.sprite.Group()#create empty update group
     drawable = pygame.sprite.Group()#create empty drawable group
+    asteroids = pygame.sprite.Group()#create asteroids group
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     Player.containers = (updatable, drawable)#add Player (from player.py class Player) to both groups then create player object
+    asteroidfield = AsteroidField()
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))#instantiate the player object to the middle of the screen
     game_running = True
     while game_running:
@@ -21,6 +29,10 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)#player controls for movement
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game Over!")
+                sys.exit()
         for sprites in drawable:
             sprites.draw(screen)#draw player on the screen using the draw method of player.py
         pygame.display.flip()
