f07033fe43aa0772493046b09af94bb1a7fd3095
saton269 <saton269@gmail.com>
2025-06-22 20:15:09 -0400

    added lives, total of 3, added scoring

--- main.py
+++ main.py
@@ -15,17 +15,20 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     clock = pygame.time.Clock()
     dt = 0
+    total_lives = 2
+    score = 0
 
     updatable = pygame.sprite.Group()#create empty update group
     drawable = pygame.sprite.Group()#create empty drawable group
     asteroids = pygame.sprite.Group()#create asteroids group
     shots = pygame.sprite.Group()#create shots group to hold all shots
+    lives = pygame.sprite.Group()
 
     Asteroid.containers = (asteroids, updatable, drawable)
     Shot.containers = (shots, updatable, drawable)
     AsteroidField.containers = (updatable)
     asteroid_field = AsteroidField()
-    Player.containers = (updatable, drawable)#add Player (from player.py class Player) to both groups then create player object
+    Player.containers = (lives, updatable, drawable)#add Player (from player.py class Player) to both groups then create player object
     
     player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))#instantiate the player object to the middle of the screen
     
@@ -38,12 +41,20 @@ def main():
 
         for asteroid in asteroids:
             if asteroid.collision(player):
-                print("Game Over!")
-                sys.exit()
+                if total_lives == 0:
+                    print("Game Over!")
+                    print(f"Final score: {score}")
+                    sys.exit()
+                else:
+                    total_lives -= 1
+                    for life in lives:
+                        life.kill()
+                    player = Player((SCREEN_WIDTH / 2),(SCREEN_HEIGHT / 2))
             for bullet in shots:
                 if asteroid.collision(bullet):
                     asteroid.split()
                     bullet.kill()
+                    score += 1
 
         screen.fill("black")
         
